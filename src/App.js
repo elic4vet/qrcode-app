b328adf5f27fddfcd45b6d7b80b35e7f6a024860
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-07-16 21:25:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,32 +9,29 @@ function App() {
           <img src={qrcode} alt="QR code" className="qrcode" />
         </div>
         <h1>Improve your front-end skills by building projects</h1>
-
         <p className="light-txt">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
-
-        <div className="attribution">
-          Challenge by{" "}
+        <span className="attribution">
+          Challenge by {""}
           <a
             href="https://www.frontendmentor.io?ref=challenge"
             target="_blank"
             rel="noreferrer"
           >
-            Frontend Mentor{" "}
+            <span className="credits"> Frontend Mentor </span>
           </a>{" "}
-          Coded by{" "}
+        </span>
+        <span className="attribution-me">
           <a
             href="https://www.linkedin.com/in/eerkekoglou/"
             target="_blank"
             rel="noreferrer"
           >
-            {" "}
-            Elisabeth Erkekoglou{" "}
+            Coded by <span className="me"> Elisabeth Erkekoglou </span>{" "}
           </a>
-          .
-        </div>
+        </span>
       </div>
     </div>
   );
