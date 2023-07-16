14d7aeb79404d1d0a82875fb15553cb52ba506c7
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-07-16 21:49:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,26 +13,26 @@ function App() {
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
-        <span className="attribution">
-          Challenge by {""}
-          <a
-            href="https://www.frontendmentor.io?ref=challenge"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span className="credits"> Frontend Mentor </span>
-          </a>{" "}
-        </span>
-        <span className="attribution-me">
-          <a
-            href="https://www.linkedin.com/in/eerkekoglou/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Coded by <span className="me"> Elisabeth Erkekoglou </span>{" "}
-          </a>
-        </span>
       </div>
+      <span className="attribution">
+        Challenge by {""}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="credits"> Frontend Mentor </span>
+        </a>{" "}
+      </span>{" "}
+      <span className="attribution-me">
+        <a
+          href="https://www.linkedin.com/in/eerkekoglou/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Coded by <span className="me"> Elisabeth Erkekoglou </span>{" "}
+        </a>
+      </span>
     </div>
   );
 }
